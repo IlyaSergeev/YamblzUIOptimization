d666e72e70965bf8a3c94426d0ba581c14935f64
Kostya Charkin <93kostya@gmail.com>
2016-08-09 02:21:37 +0300

    hw UI optimization

--- app/src/main/java/com/yamblz/uioptimizationsample/ui/ArtistsAdapter.java
+++ app/src/main/java/com/yamblz/uioptimizationsample/ui/ArtistsAdapter.java
@@ -1,9 +1,19 @@
 package com.yamblz.uioptimizationsample.ui;
 
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Shader;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +21,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 import com.yamblz.uioptimizationsample.R;
 import com.yamblz.uioptimizationsample.model.Artist;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -55,6 +68,7 @@ public class ArtistsAdapter extends RecyclerView.Adapter<ArtistsAdapter.ArtistVH
     @Override
     public void onBindViewHolder(ArtistVH holder, int position)
     {
+        holder.posterImageView.setImageDrawable(null);
         holder.bind(artists[position]);
     }
 
@@ -66,6 +80,7 @@ public class ArtistsAdapter extends RecyclerView.Adapter<ArtistsAdapter.ArtistVH
 
     public class ArtistVH extends RecyclerView.ViewHolder
     {
+        private static final int GRADIENT_HEIGHT = 250;
         @BindView(R.id.artist_poster)
         ImageView posterImageView;
 
@@ -81,15 +96,33 @@ public class ArtistsAdapter extends RecyclerView.Adapter<ArtistsAdapter.ArtistVH
         @BindView(R.id.artist_description)
         TextView descriptionTextView;
 
+        private final Target target;
+
         public ArtistVH(View itemView)
         {
             super(itemView);
             ButterKnife.bind(this, itemView);
+            target = new Target() {
+                @Override
+                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                    posterImageView.setImageBitmap(addGradient(bitmap));
+                }
+
+                @Override
+                public void onBitmapFailed(Drawable errorDrawable) {
+
+                }
+
+                @Override
+                public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                }
+            };
         }
 
         public void bind(@NonNull Artist artist)
         {
-            picasso.load(artist.getCover().getBigImageUrl()).into(posterImageView);
+            picasso.load(artist.getCover().getBigImageUrl()).into(target);
             nameTextView.setText(artist.getName());
             descriptionTextView.setText(artist.getDescription());
             albumsTextView.setText(resources.getQuantityString(R.plurals.artistAlbums,
@@ -98,6 +131,23 @@ public class ArtistsAdapter extends RecyclerView.Adapter<ArtistsAdapter.ArtistVH
             songsTextView.setText(resources.getQuantityString(R.plurals.artistTracks,
                                                               artist.getTracksCount(),
                                                               artist.getTracksCount()));
+        }
+
+        public Bitmap addGradient(Bitmap src) {
+            int w = src.getWidth();
+            int h = src.getHeight();
+            Bitmap overlay = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(overlay);
+
+            canvas.drawBitmap(src, 0, 0, null);
+
+            Paint paint = new Paint();
+            LinearGradient shader = new LinearGradient(0, 3*h/4, 0, h, 0xFFFFFFFF, 0x00FFFFFF, Shader.TileMode.CLAMP);
+            paint.setShader(shader);
+            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+            canvas.drawRect(0, 3*h/4, w, h, paint);
+
+            return overlay;
         }
     }
 }
